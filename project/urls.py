7fe4f1f8a3c460cd026331bc9815f511e65b9f30
Olga <kladof@mail.ru>
2023-06-07 20:58:04 +0300

    basedata commit

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('other/', include('other.urls')),
     path('login/', include('login.urls')),
-    path('store/', include('store.urls')),
+    path('', include('store.urls')),
 
 ]
